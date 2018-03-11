817f1f75beafb76701a17d54a3b73005f47bcd05
Ian Reynolds <idreyn@gmail.com>
2018-03-11 16:37:25 -0700

    the code is somewhat better now

---
+++ render.py
@@ -0,0 +1,127 @@
+from __future__ import division
+
+import math
+
+import numpy as np
+import robin.util
+
+SPEED_OF_SOUND = 343 # m/s
+
+
+def distance_delay_in_samples(fs, distance):
+	return int(2 * fs * distance / SPEED_OF_SOUND)
+
+
+def simple_attenuation_coeff(distance, surface_area, air_loss_coeff=None):
+	d_full_power_wavefront = 0.1
+	relative_surface_area = (surface_area / d_full_power_wavefront) ** 2
+	return relative_surface_area / (distance / d_full_power_wavefront) ** 4
+
+
+def moderate_itds_for(delays, itd_compress):
+	[left, right] = delays
+	mean = (left + right) / 2
+	difference = left - right
+	new_difference = difference / itd_compress
+	return map(int, [mean + new_difference / 2, mean - new_difference / 2])
+
+
+class EchoSource(object):
+	def __init__(self, position, surface_area=1, refraction=1):
+		self.position = position
+		self.surface_area = surface_area
+		self.refraction = refraction
+
+	def azimuth(self):
+		return (180 / math.pi) * (
+			math.pi - math.atan2(self.position[1], self.position[0]))
+
+	def distance_to_point(self, point):
+		(x1, y1), (x2, y2) = self.position, point
+		return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5	
+
+
+class HeadModel(object):
+	def __init__(self, hrtf_data_getter, ear_distance=0.1):
+		self.hrtf_data_getter = hrtf_data_getter
+		self.ear_distance = ear_distance
+
+	def ear_position_of_channel(self, channel):
+		return [
+			(-self.ear_distance, 0),
+			(self.ear_distance, 0)
+		][channel]
+
+	def apply_hrtf(self, sample, source, channel):
+		sample_at_channel = sample[:, channel]
+		input_power = np.sum(sample_at_channel ** 2)
+		imp_res = self.hrtf_data_getter(0, source.azimuth())[:, channel]
+		output = np.convolve(sample_at_channel, imp_res)
+		output_power = np.sum(output ** 2)
+		print output_power / input_power
+		print "max", np.max(output), np.max(sample)
+		return output / 100
+
+
+class OutputLayer(object):
+	def __init__(self, rnd_pulse):
+		self.rnd_pulse = rnd_pulse
+		self.CHANNELS = xrange(2)
+
+	def render(self, fs):
+		for i in self.CHANNELS:
+			yield self.rnd_pulse[:, i], 0
+
+
+class Layer(object):
+	def __init__(self, rnd_pulse, echo_source, head_model, itd_compress=1):
+		self.rnd_pulse = rnd_pulse
+		self.echo_source = echo_source
+		self.head_model = head_model
+		self.itd_compress = itd_compress
+		self.CHANNELS = xrange(2)
+
+	def render(self, fs):
+		channel_delays = []
+		samples = []
+		for channel in self.CHANNELS:
+			distance = self.echo_source.distance_to_point(
+				self.head_model.ear_position_of_channel(channel)
+			)
+			delay = distance_delay_in_samples(fs, distance)
+			attenuation = simple_attenuation_coeff(
+				distance,
+				self.echo_source.surface_area
+			)
+			channel_delays.append(delay)
+			samples.append(
+				self.head_model.apply_hrtf(
+					attenuation * self.rnd_pulse,
+					self.echo_source,
+					channel
+				)
+			)
+		for i in self.CHANNELS:
+			yield samples[i], channel_delays[i]
+
+
+class Timeline(object):
+	def __init__(self, layers=None):
+		self.layers = layers or []
+
+	def add_layer(self, layer):
+		self.layers.append(layer)
+
+	def render(self, fs):
+		scene = np.zeros((1, 2))
+		for layer in self.layers:
+			for channel, (chan_sample, delay) in enumerate(layer.render(fs)):
+				end_sample = len(chan_sample) + delay - 1
+				if len(scene) < end_sample:
+					scene = robin.util.zero_pad(scene, right_length=end_sample)
+				scene[:, channel] += robin.util.zero_pad(
+					chan_sample,
+					left_length=delay,
+					right_length=(len(scene) - len(chan_sample) - delay)
+				)
+		return scene
--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import math
+import random
 
 import matplotlib
 matplotlib.use('TkAgg')
@@ -10,11 +11,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import robin.pulse
 import robin.plotting.util
-import robin.util
-
 import hrtf
 
-SPEED_OF_SOUND = 343 # m/s
+from render import EchoSource, HeadModel, OutputLayer, Layer, Timeline
+
 
 # Shhhhhh
 class DeviceShim(object):
@@ -22,95 +22,6 @@ class DeviceShim(object):
 		self.rate = rate
 		self.channels = channels
 		self.np_format = np_format
-
-
-class EchoSource(object):
-	def __init__(self, position, surface_area=1, refraction=1):
-		self.position = position
-		self.surface_area = surface_area
-		self.refraction = refraction
-
-	def azimuth(self):
-		return (180 / math.pi) * (
-			math.pi - math.atan2(self.position[1], self.position[0]))
-
-
-class HeadModel(object):
-	def __init__(self, hrtf_data_getter, ear_distance=0.1):
-		self.hrtf_data_getter = hrtf_data_getter
-		self.ear_distance = ear_distance
-
-	def ear_positions(self):
-		return [
-			(-self.ear_distance, 0),
-			(self.ear_distance, 0)
-		]
-
-	def apply_hrtf(self, sample, source, channel):
-		input_power = np.sum(sample ** 2)
-		ir = self.hrtf_data_getter(0, source.azimuth())[:,channel]
-		output = np.convolve(sample, ir)
-		output_power = np.sum(output ** 2)
-		print output_power / input_power
-		print "max", np.max(output), np.max(sample)
-		return output / 100
-
-
-class SceneItem(object):
-	def __init__(self, sound, sample_delay=0, start_delay=0):
-		self.sound = sound
-		self.sample_delay = sample_delay
-		self.start_delay = start_delay
-
-	def render_for_scene(self, scene_duration):
-		pad_left = int(self.sample_delay + self.start_delay)
-		pad_right = int(scene_duration - pad_left - len(self.sound))
-		res = robin.util.zero_pad(self.sound, pad_left, pad_right)
-		return res
-
-
-def point_distance(a, b):
-	(x1, y1), (x2, y2) = a, b
-	return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-
-
-def distance_delay_in_samples(distance, fs):
-	return int(2 * fs * distance / SPEED_OF_SOUND)
-
-
-def simple_head_attenuation_model(position):
-	def projection_to_attenuation(p):
-		max_coeff = 1
-		min_coeff = 0.1
-		tightness = 5
-		return min_coeff + (
-			(max_coeff - min_coeff) * (
-				1 / (1 + np.exp(-1 * tightness * p))
-			)
-		)
-	norm_position = position / np.linalg.norm(position)
-	ear_normals = [
-		n / np.linalg.norm(n)
-		for n in [(-1, 0), (1, 0)]
-	]
-	projections = [
-		np.dot(norm_position, ear_normals[i])
-		for i in xrange(len(ear_normals))
-	]
-	print "PROJECTIONS", norm_position, projections
-	res = [projection_to_attenuation(p) for p in projections]
-	print "ATTN", res
-	return res
-
-
-def simple_attenuation_coeff(distance, surface_area, air_loss_coeff=None):
-	d_full_power_wavefront = 0.1
-	relative_surface_area = (surface_area / d_full_power_wavefront) ** 2
-	return relative_surface_area / (distance / d_full_power_wavefront) ** 4
-
-
-def as_channels(left, right):
-	return np.transpose(np.vstack((left, right)))
 
 
 def write_scene(scene, fs):
@@ -129,83 +40,31 @@ def plot_scene(scene, fs):
 	robin.plotting.util.plot_stereo(t_axis, scene[:,0], scene[:,1])
 
 
-def moderate_itds_for(delays, intended_slowdown):
-	[left, right] = delays
-	mean = (left + right) / 2
-	difference = left - right
-	new_difference = difference / intended_slowdown
-	return [mean + new_difference / 2, mean - new_difference / 2]
+def render_scene(device, rnd_pulse, head_model, echo_sources, 
+		us_duration, fs, itd_compress=20):
+	return Timeline([OutputLayer(rnd_pulse)] + [
+		Layer(rnd_pulse, echo_source, head_model, itd_compress)
+		for echo_source in echo_sources
+	]).render(fs)
 
 
-def render_scene(device, pulse, head, echo_sources, 
-		us_duration, fs, intended_slowdown=20):
-	rendered_pulse = 1 * pulse.render(device)
-	pulse_left, pulse_right = rendered_pulse[:,0], rendered_pulse[:,1]
-	sample_duration = int(us_duration * 1e-6 * fs)
-	start_delay = int(sample_duration / 10)
-	empty_layer = lambda: np.zeros((sample_duration, 2))
-	layers = []
-	# Add the initial pulse
-	layers.append(
-		as_channels(*[
-			SceneItem(
-				rendered_pulse,
-				start_delay=start_delay
-			).render_for_scene(sample_duration)[:,0],
-			SceneItem(
-				rendered_pulse,
-				start_delay=start_delay
-			).render_for_scene(sample_duration)[:,1],
-		])
-	)
-	channels = xrange(2)
-	ear_positions = head.ear_positions()
-	for source in echo_sources:
-		distances = [
-			point_distance(
-				source.position,
-				ear_positions[channel]
-			)
-			for channel in channels
-		]
-		delays = moderate_itds_for([
-			distance_delay_in_samples(distance, fs)
-			for distance in distances
-		], intended_slowdown)
-		attenuations = [
-			simple_attenuation_coeff(distance, source.surface_area)
-			for distance in distances
-		]
-		rendered_data = as_channels(*[
-			head.apply_hrtf(
-				SceneItem(
-					attenuations[channel] * rendered_pulse,
-					delays[channel],
-					start_delay
-				).render_for_scene(sample_duration)[:,channel],
-				source,
-				channel
-			)
-			for channel in channels
-		])
-		layers.append(rendered_data)
-	result = empty_layer()
-	for layer in layers:
-		result = robin.util.zero_pad(
-			result, right_length=(len(layer) - len(result))) + layer
-	return result
-
-
-ECHO_SOURCES = [
-	EchoSource((1 * (i + 1) * np.cos(a), 1 * (i + 1) * np.sin(a)), i)
+CIRCULAR_ECHO_SOURCES = [
+	EchoSource((1 * (i + 1) * np.cos(a), 1 * (i + 1) * np.sin(a)), i ** 0.75)
 	for i, a in enumerate(np.linspace(0, np.pi, 10))
 ]
 
-
 ECHO_SOURCES = [
-EchoSource((0, 3), 1),
+	EchoSource((0, 3), 1),
 	EchoSource((3, 3), 3),
 ]
+
+def generate_random_echo_sources(n=5, dist_bounds=(1,5), sa_bounds=(0.5, 1)):
+	for i in xrange(n):
+		azm = random.random() * 180
+		dist = dist_bounds[0] + random.random() * dist_bounds[1]
+		pos = (dist * np.cos(azm), dist * np.sin(azm))
+		sa = sa_bounds[0] + random.random() * sa_bounds[1]
+		yield EchoSource(pos, sa)
 
 
 if __name__ == "__main__":
@@ -213,7 +72,8 @@ if __name__ == "__main__":
 	f0, f1, dur = 1.0e4, 2.0e4, 1e6 * 0.005
 	np_format = np.int16
 	device = DeviceShim(fs, 2, np_format)
+	chirp = robin.pulse.Chirp(f0, f1, dur).render(device)
 	head = HeadModel(hrtf.make_hrtf_data_getter(fs))
-	chirp = robin.pulse.Chirp(f0, f1, dur)
-	scene = render_scene(device, chirp, head, ECHO_SOURCES, 1e5, fs, 50)
+	echo_sources = generate_random_echo_sources()
+	scene = render_scene(device, chirp, head, echo_sources, 1e5, fs, 20)
 	write_scene(scene, fs)
