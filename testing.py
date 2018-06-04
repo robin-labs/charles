41e5c84556bc89975ef087d28c8658d2f8497305
Ian Reynolds <idreyn@gmail.com>
2018-06-03 19:37:32 -0700

    testing w/charles

--- testing.py
+++ testing.py
@@ -2,57 +2,33 @@ from __future__ import division
 
 import math
 
+import charles.head_model
+import charles.raycast
+import charles.scene
+import hrtf
 import numpy as np
 import robin.pulse
 import robin.plotting.util
-import hrtf
 
 from scene import EchoSource, HeadModel
 from render import EchoLayer, Layer, Timeline, render_scene
 from util import DeviceShim, write_scene
 from thaler_click import ExpertClick
 
-
-CIRCULAR_ECHO_SOURCES = [
-    EchoSource((1 * math.sqrt(i + 1) * np.cos(a), 1 * math.sqrt(i + 1) * np.sin(a)), 1)
-    for i, a in enumerate(np.linspace(0, np.pi, 10))
-]
-
-REVERSE_CIRCULAR_ECHO_SOURCES = [
-    EchoSource((-1 * math.sqrt(i + 1) * np.cos(a), 1 * math.sqrt(i + 1) * np.sin(a)), 1)
-    for i, a in enumerate(np.linspace(0, np.pi, 10))
-]
-
-ECHO_SOURCES = [
-    EchoSource((0, 3), 4),
-    EchoSource((3, 3), 8),
-    EchoSource((1, 1.5), 4),
-    EchoSource((-1.5, 1.5), 4),
-]
-
-def single_echo_source(dist, az, area=1):
-    return [EchoSource((-dist * np.cos(az), dist * np.sin(az)), area)]
-
 fs = 96000
 f0, f1, dur = 1.0e4, 2.2e4, 1e6 * 0.001
 np_format = np.float64
 device = DeviceShim(fs, 2, np_format)
-pulse = ExpertClick()
+pulse = robin.pulse.Chirp(f0, f1, dur)
 head = HeadModel(hrtf.make_hrtf_data_getter(fs)[0])
 
-print pulse
+wall = charles.scene.Wall((-1, 2), (1, 2), 3)
+wall2 = charles.scene.Wall((1, 1), (1, -1), 3)
+echo_sources = []
+for (position, coeff) in charles.raycast.cast_lambertian(
+    charles.head_model.HeadModel(), [wall, wall2]
+):
+    echo_sources.append(EchoSource(position[0:2], 1, coeff))
 
-def write_scenes(name):
-    for az in np.linspace(0, 180, 19):
-        az_rad = az * np.pi / 180
-        az_disp = az - 90
-        for dist in (1.5, 2, 3):
-            echo_sources = single_echo_source(dist, az_rad)
-            scene = render_scene(fs, device, pulse, head, echo_sources, 1)
-            write_scene(fs, scene, "output/{0}_{1}m_{2}.wav".format(
-                name, dist, az_disp
-            ))
-
-
-if __name__ == "__main__":
-    write_scenes("ee1")
+scene = render_scene(fs, device, pulse, head, echo_sources, 1)
+write_scene(fs, scene, "output/test_charles_chirp.wav")
